614984b717c91b10fc2663070fbaa0b115f54462
chaZz <charles.vangysel@upslide.com>
2026-09-09 14:21:07 +0200

    Fix Windows notification warnings

--- src/notifications.rs
+++ src/notifications.rs
@@ -139,7 +139,7 @@ async fn send_notification_bell(store: &AsyncProgramStore) {
 }
 
 #[cfg(feature = "desktop")]
-#[cfg_attr(target_os = "macos", allow(unused_variables))]
+#[cfg_attr(any(not(unix), target_os = "macos"), allow(unused_variables))]
 async fn send_notification_desktop(
     summary: &str,
     body: Option<&str>,
